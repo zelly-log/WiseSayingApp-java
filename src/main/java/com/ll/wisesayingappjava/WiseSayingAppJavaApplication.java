1d73227adca8f50a127dbb13a0ab8904f361eab5
zelly-log <dearmyjaei@gmail.com>
2025-02-18 15:53:28 +0900

    chore: 프로젝트 초기 설정 및 Git 연동

---
+++ src/main/java/com/ll/wisesayingappjava/WiseSayingAppJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ll.wisesayingappjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseSayingAppJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiseSayingAppJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/wisesayingappjava/WiseSayingAppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.wisesayingappjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseSayingAppJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
