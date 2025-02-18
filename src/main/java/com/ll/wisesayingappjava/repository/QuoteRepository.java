618dcb96108320346922f015675d6da2f3cc936d
zelly-log <dearmyjaei@gmail.com>
2025-02-18 16:31:00 +0900

    refactor: change directory

--- src/main/java/com/ll/wisesayingappjava/repository/QuoteRepository.java
+++ src/main/java/com/ll/wisesayingappjava/repository/QuoteRepository.java
@@ -1,4 +1,4 @@
-package com.ll.wisesayingappjava.service;
+package com.ll.wisesayingappjava.repository;
 
 import com.ll.wisesayingappjava.model.Quote;
 import org.springframework.data.jpa.repository.JpaRepository;
