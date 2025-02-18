7590ac77cc40483e137dc2e85e09c715988e031a
zelly-log <dearmyjaei@gmail.com>
2025-02-18 16:39:20 +0900

    feat: 최신순, 특정 검색어 포함 명언 목록 조회 기능 추가

--- src/main/java/com/ll/wisesayingappjava/repository/QuoteRepository.java
+++ src/main/java/com/ll/wisesayingappjava/repository/QuoteRepository.java
@@ -1,8 +1,16 @@
 package com.ll.wisesayingappjava.repository;
 
 import com.ll.wisesayingappjava.model.Quote;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
+
+    // 최신 순으로 명언 목록 조회(한 페이지에 최대 5개 페이징)
+    Page<Quote> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+    // 특정 검색어를 포함하는 명언 목록 조회(한 페이지에 최대 5개 페이징)
+    Page<Quote> findByAuthorContainingOrContentContainingOrderByCreatedAtDesc(String author, String content, Pageable pageable);
 
 }
