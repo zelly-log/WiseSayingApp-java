4254694600397414d3ab1023d6007c739d4ef003
zelly-log <dearmyjaei@gmail.com>
2025-02-19 12:55:48 +0900

    feat: 명언 목록 조회, 저장 기능 구현

--- src/main/java/com/ll/wisesayingappjava/service/QuoteService.java
+++ src/main/java/com/ll/wisesayingappjava/service/QuoteService.java
@@ -1,16 +1,39 @@
 package com.ll.wisesayingappjava.service;
 
 import com.ll.wisesayingappjava.model.Quote;
+import com.ll.wisesayingappjava.repository.QuoteRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface QuoteService {
+@RequiredArgsConstructor
+public class QuoteService {
+
+    private final QuoteRepository quoteRepository;
+
+    // 명언 저장 기능
+    public Quote savedQuote(String author, String content){
+        Quote quote = new Quote(null, author, content, LocalDateTime.now());
+        return quoteRepository.save(quote);
+    }
+
+    // 명언 목록 조회 (페이징 처리 x)
+    public List<Quote> getAllQuotesList(){
+        return quoteRepository.findAll();
+    }
 
     // 명언 목록 조회(페이징 처리)
-    Page<Quote> getAllQuotes(Pageable pageable);
+    public Page<Quote> getAllQuotes(Pageable pageable){
+        return quoteRepository.findAllByOrderByCreatedAtDesc(pageable);
+    };
 
     // 검색 기능 추가(페이징 처리)
-    Page<Quote> searchQuotes(String keyword, Pageable pageable);
+    public Page<Quote> searchQuotes(String keyword, Pageable pageable){
+        return quoteRepository.searchBykeword(keyword,pageable);
+    };
+
 }
