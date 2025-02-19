4dd9bbdfdb7ead8954603cbd25bfd22a6c4fb79d
zelly-log <dearmyjaei@gmail.com>
2025-02-19 12:51:53 +0900

    feat: App 실행 기능 구현

--- src/main/java/com/ll/wisesayingappjava/WiseSayingAppJavaApplication.java
+++ src/main/java/com/ll/wisesayingappjava/WiseSayingAppJavaApplication.java
@@ -2,12 +2,21 @@ package com.ll.wisesayingappjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
 public class WiseSayingAppJavaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WiseSayingAppJavaApplication.class, args);
+        ApplicationContext context = SpringApplication.run(WiseSayingAppJavaApplication.class, args);
+
+        // App 실행
+        App app = context.getBean(App.class);
+        app.run();
+
     }
+
+
 
 }
