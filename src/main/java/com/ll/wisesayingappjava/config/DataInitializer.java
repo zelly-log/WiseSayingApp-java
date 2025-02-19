5fc223c96a4030d604bcb59c0b20172d08f4ebae
zelly-log <dearmyjaei@gmail.com>
2025-02-19 12:48:42 +0900

    refactor: Add Sample data

--- src/main/java/com/ll/wisesayingappjava/config/DataInitializer.java
+++ src/main/java/com/ll/wisesayingappjava/config/DataInitializer.java
@@ -20,16 +20,18 @@ public class DataInitializer implements CommandLineRunner {
     public void run(String... args){
         if(quoteRepository.count() == 0){ // 데이터 없는 경우에 초기화(데이터 있음 추가 x)
             List<Quote> quotes = List.of(
-                new Quote(null, "작자미상", "명언 10", LocalDateTime.now()),
-                new Quote(null, "작자미상", "명언 9", LocalDateTime.now()),
-                new Quote(null, "작자미상", "명언 8", LocalDateTime.now()),
-                new Quote(null, "작자미상", "명언 7", LocalDateTime.now()),
-                new Quote(null, "작자미상", "명언 6", LocalDateTime.now()),
-                new Quote(null, "작자미상", "명언 5", LocalDateTime.now()),
-                new Quote(null, "작자미상", "명언 4", LocalDateTime.now()),
-                new Quote(null, "작자미상", "명언 3", LocalDateTime.now()),
+                new Quote(null, "작자미상", "명언 1", LocalDateTime.now()),
                 new Quote(null, "작자미상", "명언 2", LocalDateTime.now()),
-                new Quote(null, "작자미상", "명언 1", LocalDateTime.now())
+                new Quote(null, "작자미상", "명언 3", LocalDateTime.now()),
+                new Quote(null, "작자미상", "명언 4", LocalDateTime.now()),
+                new Quote(null, "작자미상", "명언 5", LocalDateTime.now()),
+                new Quote(null, "작자미상", "명언 6", LocalDateTime.now()),
+                new Quote(null, "작자미상", "명언 7", LocalDateTime.now()),
+                new Quote(null, "작자미상", "명언 8", LocalDateTime.now()),
+                new Quote(null, "작자미상", "명언 9", LocalDateTime.now()),
+                new Quote(null, "작자미상", "명언 10", LocalDateTime.now()),
+                new Quote(null, "테스트다", "졸려", LocalDateTime.now())
+
             );
             quoteRepository.saveAll(quotes);
         }
