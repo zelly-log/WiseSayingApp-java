e497d0e3d9b3ac7084e4e19fc89a3afe4061fff2
zelly-log <dearmyjaei@gmail.com>
2025-02-19 12:57:11 +0900

    refactor: 특정 검색어 포함 명언 조회 쿼리문 수정

--- src/main/java/com/ll/wisesayingappjava/repository/QuoteRepository.java
+++ src/main/java/com/ll/wisesayingappjava/repository/QuoteRepository.java
@@ -4,6 +4,8 @@ import com.ll.wisesayingappjava.model.Quote;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
 
@@ -11,6 +13,7 @@ public interface QuoteRepository extends JpaRepository<Quote, Long> {
     Page<Quote> findAllByOrderByCreatedAtDesc(Pageable pageable);
 
     // 특정 검색어를 포함하는 명언 목록 조회(한 페이지에 최대 5개 페이징)
-    Page<Quote> findByAuthorContainingOrContentContainingOrderByCreatedAtDesc(String author, String content, Pageable pageable);
+    @Query("SELECT q from Quote q where q.author like %:keyword% or q.content like %:keyword%")
+    Page<Quote> searchBykeword(@Param("keyword") String keyword, Pageable pageable);
 
 }
